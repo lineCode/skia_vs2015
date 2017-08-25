7004059a339db0c8918610c6746d33034fe20b70
liuxiufeng <liuxiufeng@qiyi.com>
2017-08-25 15:19:51 +0800

    use vs2015 to compile skia dll

---
+++ start_vs2015.bat
@@ -0,0 +1,7 @@
+@echo off
+@rem
+@set depot_tools_path=D:\code\iqiyi\QisuClient\third_party\google\depot_tools
+@set python_path=C:\Python27
+@set PATH=%PATH%;%depot_tools_path%;%python_path%;
+@"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+@cmd /k
